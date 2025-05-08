a69383ce2c9ff94affa720fb81a6559d42c984bc
Millie <57187866+unamimo@users.noreply.github.com>
2025-05-08 13:30:08 +0100

    chore: add timer class

--- CustomLogger/CustomTimer.h
+++ CustomLogger/CustomTimer.h
@@ -1,15 +1,40 @@
 #pragma once
 
 #include <chrono>		// getting current time
-#include "CustomLogger.h"
 
-template<typename func, typename... Args>
-
-void getFunctionTime(func f, Args&&... args)
+class Timer
 {
-    auto start = std::chrono::high_resolution_clock::now();
-    f(std::forward<Args>(args)...);
-    auto end = std::chrono::high_resolution_clock::now();
+public:
+    // Starts a timer
+    // must call stopTimer after using start
+    void startTimer()
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
 
-    std::cout << "took " << duration_cast<std::chrono::seconds>(end - start).count() << " seconds" << std::endl;
-}
+    // stops the timer
+    void stopTimer()
+    {
+        end = std::chrono::high_resolution_clock::now();
+    }
+
+    // outputs time taken in microseconds
+    void outputTimeTaken()
+    {
+        std::cout << "took " << duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds" << "\n\n";
+    }
+
+private:
+    std::chrono::steady_clock::time_point start;
+    std::chrono::steady_clock::time_point end;
+};
+
+//template<typename func, typename... Args>
+//void getFunctionTime(func f, Args&&... args)
+//{
+//    auto start = std::chrono::high_resolution_clock::now();
+//    f(std::forward<Args>(args)...);
+//    auto end = std::chrono::high_resolution_clock::now();
+//
+//    std::cout << "took " << duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds" << std::endl;
+//}
--- CustomLogger/main.cpp
+++ CustomLogger/main.cpp
@@ -6,61 +6,50 @@ int main()
 {
     // Choose file to log to
     CustomLogger mCustomLogger("LogFile.txt");
+    Timer mTimer;
 
 
     // Logging with different variable types
-    auto start1 = std::chrono::high_resolution_clock::now();
-
+    mTimer.startTimer();
     mCustomLogger.LogToConsole("Hello world!", 1, 2.4, 33333, true, "a");
-
-    auto end1 = std::chrono::high_resolution_clock::now();
-    std::cout << "took " << duration_cast<std::chrono::microseconds>(end1 - start1).count() << " microseconds" << std::endl;
+    mTimer.stopTimer();
+    mTimer.outputTimeTaken();
 
 
     // Logging to file
-    auto start2 = std::chrono::high_resolution_clock::now();
-    
+    mTimer.startTimer();
     mCustomLogger.LogToFile("Hello", "World", "!", 1, 2, 4.4, 56745674);
-
-    auto end2 = std::chrono::high_resolution_clock::now();
-    std::cout << "took " << duration_cast<std::chrono::microseconds>(end2 - start2).count() << " microseconds" << std::endl;
+    mTimer.stopTimer();
+    mTimer.outputTimeTaken();
 
 
     // Log current time
-    auto start3 = std::chrono::high_resolution_clock::now();
-    
+    mTimer.startTimer();
     mCustomLogger.LogToConsole(mCustomLogger.getCurrentTime());
-
-    auto end3 = std::chrono::high_resolution_clock::now();
-    std::cout << "took " << duration_cast<std::chrono::microseconds>(end3 - start3).count() << " microseconds" << std::endl;
+    mTimer.stopTimer();
+    mTimer.outputTimeTaken();
 
 
     // Log current time and message
-    auto start4 = std::chrono::high_resolution_clock::now();
-
+    mTimer.startTimer();
     mCustomLogger.LogToConsole(mCustomLogger.getCurrentTime(), "Log with timestamp");
-
-    auto end4 = std::chrono::high_resolution_clock::now();
-    std::cout << "took " << duration_cast<std::chrono::microseconds>(end4 - start4).count() << " microseconds" << std::endl;    auto start = std::chrono::high_resolution_clock::now();
+    mTimer.stopTimer();
+    mTimer.outputTimeTaken();
 
 
     // Logging with iteration
-    auto start5 = std::chrono::high_resolution_clock::now();
-
+    mTimer.startTimer();
     for (size_t i = 0; i < 11; i++)
     {
         mCustomLogger.LogToConsole("Logging in a loop:", i);
     }
-
-    auto end5 = std::chrono::high_resolution_clock::now();
-    std::cout << "took " << duration_cast<std::chrono::microseconds>(end5 - start5).count() << " microseconds" << std::endl;    
+    mTimer.stopTimer();
+    mTimer.outputTimeTaken();
 
 
     // Logging based on condition
-    auto start6 = std::chrono::high_resolution_clock::now();
- 
+    mTimer.startTimer();
     mCustomLogger.LogToConsole(mCustomLogger.testVal == 8 ? "True" : "False");
-
-    auto end6 = std::chrono::high_resolution_clock::now();
-    std::cout << "took " << duration_cast<std::chrono::microseconds>(end6 - start6).count() << " microseconds" << std::endl;
+    mTimer.stopTimer();
+    mTimer.outputTimeTaken();
 }
